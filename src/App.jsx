354bfc313f4d7d6ad3407d2b8dec0b6ebf958487
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-01-22 17:02:00 -0500

    added tailwindcss dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Your Kanban Board</h1>
+      <h1 className="font-bold text-4xl">Your Kanban Board</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p className="text-3xl font-bold">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
