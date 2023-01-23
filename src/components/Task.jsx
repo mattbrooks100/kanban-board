daf414ff96645a22baae00e033ecf042511760a8
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2023-01-23 10:58:47 -0500

    formatted Task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const Task = ({ task }) => {
+  // Convert the date to a more human-friendly version
+  const newDate = new Date(task.due_date);
+  newDate.setDate(newDate.getDate() + 1);
+  const formattedDate = newDate.toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
   return (
     <div className="my-4 p-2 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl rounded-lg">
-      <p>{task.description}</p>
-      <p>{task.due_date}</p>
+      <div className="text-xl font-semibold">{task.description}</div>
+      <div>Due by: {formattedDate}</div>
     </div>
   );
 };
